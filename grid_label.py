1337a1c6e90b0d695afa931a1af5ee2d095616bc
gabepetersen <precariousplant@gmail.com>
2020-01-14 20:19:27 -0800

    added program for user to identify smoke regions while preprocessing data

---
+++ grid_label.py
@@ -0,0 +1,135 @@
+# grid_label.py
+# Gabe Petersen
+# 13 Jan 2020
+# To go thru a frame of a video and label each section as having smoke or not
+
+# for a image of 480x270, 16 boxes in width and 9 in height will result in grid squares of 30 pixels each
+
+import cv2
+import keyboard
+import argparse
+import sys
+import numpy as np
+
+# declare a 2d array and initialize all entries to 0
+smoke_regions = [[0 for x in range(9)] for y in range(16)]
+# global redraw condition
+cond_redraw = True
+
+# reads in a video using opencv's VideoCapture object
+def read_video(filename):
+    # Create a VideoCapture object and read from input file
+    video = cv2.VideoCapture(filename)
+
+    # Check if video opened successfully
+    if (video.isOpened() == False):
+        print("Error opening video stream or file")
+    else:
+        # return video
+        return video
+
+# redraws the data structure onto the image for visual clarification
+def redrawBoxes(curr_frame):
+    frame_copy = curr_frame.copy()
+    # go thru the smoke_regions and draw rectangles on the frame copy
+    for x in range(0, 16):
+        for y in range(0, 9):
+            if smoke_regions[x][y] == 255:
+                xcord = x * 120
+                ycord = y * 120
+                frame_copy = cv2.rectangle(frame_copy, (xcord, ycord), (xcord+120, ycord+120), (0,255,0), 2)
+
+    return frame_copy
+
+
+# event handler for mouseclicking
+def click_box(event, x, y, flags, param):
+    # mouse was clicked and region is selected
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # see what box in the data structure
+        gridX = int(x / 120)
+        gridY = int(y / 120)
+        print("Here is the coordinates you chose yo!  " + str(gridX) + ", " + str(gridY))
+
+        # change between 0 and 255 depending on previous state
+        if(smoke_regions[gridX][gridY] == 0):
+            smoke_regions[gridX][gridY] = 255
+        else:
+            smoke_regions[gridX][gridY] = 0
+
+        # set redraw bool to true
+        cond_redraw = True
+
+# main function responsible for displaying frames and saving user inputted data
+def grid_label(dataInputPath, videoCount):
+    # create a VideoCapture object
+    video = read_video(dataInputPath)
+    # create a video writer for smoke_regions
+    dataPath = 'GridDataResults/' + str(videoCount) + '.mov'
+    fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
+    videoWrite = cv2.VideoWriter(dataPath, fourcc, 10, (16, 9) )
+    # start the frame sequence
+    frame_sequence(video, videoWrite)
+
+    # release the video read and write
+    video.release()
+    videoWrite.release()
+    cv2.destroyAllWindows()
+
+# goes thru video sequence and displays frames
+def frame_sequence(video, videoWrite):
+    # go through each of the frames
+    frame_count = 0
+    done = False
+    while not done:
+        frame_count += 1
+        ret, curr_frame = video.read()
+        frame_copy = redrawBoxes(curr_frame)
+
+        # set event handler for mouseclick
+        cv2.namedWindow("Current Frame")
+        cv2.setMouseCallback("Current Frame", click_box)
+
+        # displays the image and waits for clicks, next frame, or quit
+        while True:
+            # display the image
+            cv2.imshow("Current Frame", frame_copy)
+            key = cv2.waitKey(1) & 0xFF
+
+            frame_copy = curr_frame.copy()
+            # go thru the smoke_regions and draw rectangles on the frame copy
+            for x in range(0, 16):
+                for y in range(0, 9):
+                    if smoke_regions[x][y] == 255:
+                        xcord = x * 120
+                        ycord = y * 120
+                        frame_copy = cv2.rectangle(frame_copy, (xcord, ycord), (xcord+120, ycord+120), (0,255,0), 2)
+
+            if key == ord("n"):
+                print("next frame")
+                # write the current data structure to the output video
+                region_data = np.uint8(smoke_regions)
+                videoWrite.write(region_data)
+                print("region data: ", region_data)
+                break
+            if key == ord("q"):
+                # exit the program
+                done = True
+                break
+
+if __name__ == "__main__":
+    # get the video count from command line
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-n", "--number", required=True, help="Video Number in Data Folder")
+    args = vars(ap.parse_args())
+
+    videoCount = args["number"]
+
+    dataInputPath = "Data/" + videoCount + ".mp4"
+    print("From video file: " + dataInputPath)
+    # start grid label sequence
+    print("Instructions:")
+    print("   - while on the image feed, click to select and unselect sections of smoke")
+    print("   - after selection is made, hit n to write to file and go to next frame")
+    print("   - hit q any time to quit while on the video")
+    grid_label(dataInputPath, videoCount)
--- smoke_detect.py
+++ smoke_detect.py
@@ -29,61 +29,102 @@ def smoke_segment(dataInputPath):
 
     # release the video
     video.release()
+    cv2.destroyAllWindows()
 
 # optical flow motion detection
 # subtracts two frames and evaluates if entire frame is moving or not
 def smoke_segment_helper(video):
     # read the first frame
     ret, prev_frame = video.read()
-    # apply the low saturation filter on the first frame
-    prev_frame = hsv_filter(prev_frame)
-
     prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+
     # go through each of the frames
     while True:
         ret, curr_frame = video.read()
         if ret:
+            # blur this!
+            # kernel = np.ones((3,3), np.float32) / 9
+            # curr_frame = cv2.filter2D(curr_frame, -1, kernel)
+            curr_frame = cv2.GaussianBlur(curr_frame, (3,3), 0)
+
+            # morphological smoothing
+            kernel = np.ones((15,15), np.uint8)
+            #curr_frame = cv2.morphologyEx(curr_frame, cv2.MORPH_OPEN, kernel)
+
             # show the current frame in the video before filtering
             cv2.imshow("before saturation filter", curr_frame)
-            # apply the low saturation filter on the current frame
-            curr_frame = hsv_filter(curr_frame)
-            # show the saturation filtered frame
-            cv2.imshow("filtered saturation frame", curr_frame)
+
             # convert the frame to one channel
             curr_gray = cv2.cvtColor(curr_frame, cv2.COLOR_BGR2GRAY)
-            # conduct some good ol optical flow
-            opFlow = cv2.calcOpticalFlowFarneback(prev_gray, curr_gray, None, 0.5, 3, 15, 3, 7, 1.5, 0)
 
-            # create images for x and y optical flow
-            opFlowX = np.zeros((IMG_H, IMG_W, 1), dtype="uint8")
-            opFlowY = np.zeros((IMG_H, IMG_W, 1), dtype="uint8")
-            # initialize the opFlowX and opFlowY
-            for x in range(IMG_W):
-                for y in range(IMG_H):
-                    opFlowX[y,x] = int(abs(100*opFlow[y,x][0]))
-                    opFlowY[y,x] = int(abs(100*opFlow[y,x][1]))
+            # calculate the optical flow
+            op_flow(curr_gray, prev_gray, curr_frame)
 
-            # show optical flow in x and y directions
-            cv2.imshow("optical flow x", opFlowX)
-            cv2.imshow("optical flow y", opFlowY)
-            # return op_flow
             if cv2.waitKey(30) & 0xFF == ord('q'):
                 break
             # update previous frame
-            #prev_frame = curr_frame
-            #prev_gray = curr_gray
+            prev_gray = curr_gray
+
+def op_flow(curr_gray, prev_gray, curr_frame):
+        # conduct some good ol optical flow
+        opFlow = cv2.calcOpticalFlowFarneback(prev_gray, curr_gray, None, 0.5, 3, 15, 3, 7, 1.5, 0)
+
+        # create images for x and y optical flow
+        opFlowX, opFlowY = cv2.split(opFlow)
+        # convert to CV_8U
+        opFlowY *= 10
+        opFlowY = np.uint8(opFlowY)
+        # show the optical flow of y
+        cv2.imshow("optical flow y", opFlowY)
+
+        # canny edge detector
+        frameEdges = cv2.Canny(curr_frame, 20, 60, 1)
+        cv2.imshow("canny edge detect", frameEdges)
+
+        # this line is almost unnecessary but its there - so there
+        ret, smokeSegments = cv2.threshold(opFlowY, 200, 255, cv2.THRESH_BINARY)
+
+        # thresh_frame = hsv_thresh(curr_frame)
+        # cv2.imshow("thresh frame", thresh_frame)
+        # hsv filter to further eliminate false positives
+        smokeSegments = hsv_filter(curr_frame, smokeSegments, 10)
+
+        # show final segmented optical flow
+        cv2.imshow("smoke segments", smokeSegments)
+
+# threshold a frame from median value of saturation
+def hsv_thresh(iframe):
+    min = 256
+    max = 0
+    frame_hsv = cv2.cvtColor(iframe, cv2.COLOR_BGR2HSV)
+    for x in range(IMG_W):
+        for y in range(IMG_H):
+            # find min and max of saturation values in image
+            if(frame_hsv[y,x][1] > max):
+                max = frame_hsv[y,x][1]
+            if(frame_hsv[y,x][1] < min):
+                min = frame_hsv[y,x][1]
+    median = (max + min) / 2
+    for x in range(IMG_W):
+        for y in range(IMG_H):
+            # find min and max of saturation values in image
+            if(frame_hsv[y,x][1] > median):
+                frame_hsv[y,x][1] = 255
+            else:
+                frame_hsv[y,x][1] = 0
+    return cv2.cvtColor(frame_hsv, cv2.COLOR_HSV2BGR)
 
 # filter out high saturation pixels
-def hsv_filter(frame):
+def hsv_filter(iframe, oframe, thresh):
     # convert frames to HSV
-    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    frame_hsv = cv2.cvtColor(iframe, cv2.COLOR_BGR2HSV)
     # for each pixel in the frame
     for x in range(IMG_W):
         for y in range(IMG_H):
             # color 0 if too high in saturation
-            if(frame_hsv[y,x][1] > 50):
-                frame[y,x] = 0
-    return frame
+            if(frame_hsv[y,x][1] > thresh):
+                oframe[y,x] = 0
+    return oframe
 
 if __name__ == "__main__":
     # get the video count from command line
