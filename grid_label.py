decadb951dbe69cfb3491d43519d7b7ae03a879e
gabepetersen <precariousplant@gmail.com>
2020-02-04 15:12:12 -0800

    changed grid_lable.py to output grid squares into /FrameExtractor/ImageSegments/...

--- grid_label.py
+++ grid_label.py
@@ -49,7 +49,7 @@ def click_box(event, x, y, flags, param):
         # see what box in the data structure
         gridX = int(x / 120)
         gridY = int(y / 120)
-        #print("Here is the coordinates you chose yo!  " + str(gridX) + ", " + str(gridY))
+        # print("Here is the coordinates you chose yo!  " + str(gridX) + ", " + str(gridY))
 
         # change between 0 and 255 depending on previous state
         if(smoke_regions[gridX][gridY] == 0):
@@ -63,11 +63,12 @@ def grid_label(dataInputPath, videoCount):
     video = read_video(dataInputPath)
     # create a video writer for smoke_regions
     dataPath = 'GridDataResults/' + str(videoCount) + '.mov'
+
     fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
     videoWrite = cv2.VideoWriter(dataPath, fourcc, 10, (16, 9), False)
 
     # start the frame sequence
-    frame_sequence(video, videoWrite)
+    frame_sequence(video, videoWrite, videoCount)
 
     # release the video read and write
     video.release()
@@ -75,7 +76,8 @@ def grid_label(dataInputPath, videoCount):
     cv2.destroyAllWindows()
 
 # goes thru video sequence and displays frames
-def frame_sequence(video, videoWrite):
+# Warning: This function is dreadfully long, sorry y'all
+def frame_sequence(video, videoWrite, videoCount):
     # declare boolean pause/play condition
     vidPlay = False
     # go through each of the frames
@@ -84,13 +86,15 @@ def frame_sequence(video, videoWrite):
     while not done:
         frame_count += 1
         ret, curr_frame = video.read()
+        if not ret:
+            done = True
+            break
         frame_copy = redrawBoxes(curr_frame)
-
         # set event handler for mouseclick
         cv2.namedWindow("Current Frame")
         cv2.setMouseCallback("Current Frame", click_box)
         # displays the image and waits for clicks, next frame, or quit
-        while True:
+        while True and ret:
             # display the image
             cv2.imshow("Current Frame", frame_copy)
             key = cv2.waitKey(1) & 0xFF
@@ -114,8 +118,6 @@ def frame_sequence(video, videoWrite):
                 region_data = cv2.rotate(region_data, cv2.ROTATE_90_COUNTERCLOCKWISE)
                 # resize for video compatibility sanity check
                 region_data = cv2.resize(region_data, (16, 9))
-                # write frame to movie
-                videoWrite.write(region_data)
                 break
             # clear the squares function
             if key == ord("c"):
@@ -137,22 +139,60 @@ def frame_sequence(video, videoWrite):
             # break loop if vidPlay is True
             if vidPlay:
                 break
+        # if the program isnt done, write data
+        if not done:
+            # write frame to movie
+            videoWrite.write(region_data)
+            imageDataPath = 'FrameExtractor/ImageSegments/'
+            # write image frames for convolutions
+            for x in range(0, 16):
+                for y in range(0, 9):
+                    # edit file names based on whether there is smoke or not in the frame
+                    if smoke_regions[x][y] == 255:
+                        imageDataPath = 'FrameExtractor/ImageSegments/Smoke/' + str(videoCount) + '_' + str(frame_count) + '_' + str(x) + '_' + str(y) + '_' + str(1) + '.jpg'
+                    else:
+                        imageDataPath = 'FrameExtractor/ImageSegments/NoSmoke/' + str(videoCount) + '_' + str(frame_count) + '_' + str(x) + '_' + str(y) + '_' + str(0) + '.jpg'
+                    # crop all the grid squares and save them as images
+                    xg = x * 120
+                    yg = y * 120
+                    grid_segment = curr_frame[yg:yg+120, xg:xg+120]
+                    cv2.imwrite(imageDataPath, grid_segment)
 
 if __name__ == "__main__":
-    # get the video count from command line
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-n", "--number", required=True, help="Video Number in Data Folder")
-    args = vars(ap.parse_args())
+    # get the folder containing all the videos from the user
+    # Enter the filepath relative to this dir where videos + HashedVideoNames.txt is stored below
+    ######
+    ###### Adjust input directory as needed
+    ######
+    filepath = "FrameExtractor/ProcessedVideos/"
+    ######
+    ###### Adjust input directory as needed
+    ######
 
-    videoCount = args["number"]
+    # Test if the directory path is valid by trying to open a HashedVideoNames.txt within it
+    try:
+        videoNames = open((filepath + 'HashedVideoNames.txt'), encoding = 'utf-8')
+    except IOError as e:
+        print('Error opening the HashedVideoNames.txt at that file path: ' + str(e))
+    else:
+        # start grid label sequence
+        print("Instructions:")
+        print("   - while on the image feed, click to select and unselect sections of smoke")
+        print("   - c - clear selection of squares")
+        print("   - n - write to file frame by frame")
+        print("   - p - write to file continuously until p is presssed again to 'pause'")
+        print("   - q - any time to quit while on the video")
 
-    dataInputPath = "Data/" + videoCount + ".mp4"
-    print("From video file: " + dataInputPath)
-    # start grid label sequence
-    print("Instructions:")
-    print("   - while on the image feed, click to select and unselect sections of smoke")
-    print("   - c - clear selection of squares")
-    print("   - n - write to file frame by frame")
-    print("   - p - write to file continuously until p is presssed again to 'pause'")
-    print("   - q - any time to quit while on the video")
-    grid_label(dataInputPath, videoCount)
+
+        # read the lines from HashedVideoNames.txt
+        videoNameList = videoNames.read().splitlines()
+        # label all those videos from the user specified directory
+        for vidname in videoNameList:
+            dataInputPath = filepath + vidname
+            vidnameSplit = vidname.split('.')
+            videoCount = vidnameSplit[0]
+            print("From video file: " + dataInputPath)
+            # call the label sequence for that video
+            grid_label(dataInputPath, videoCount)
+    finally:
+        videoNames.close()
