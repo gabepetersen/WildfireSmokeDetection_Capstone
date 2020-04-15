59b08b6ed6ddce298d4edd5d8e80c84ef714c93f
gabepetersen <precariousplant@gmail.com>
2020-04-15 03:08:28 -0700

    created a simple version 2 keras model

---
+++ keras_model_v2_test.py
@@ -0,0 +1,97 @@
+# model_gabe_test.py
+# 14 Apr 2020
+# Test the model that is trained seperately
+
+import cv2
+import numpy as np
+import sys
+import os
+import matplotlib.pyplot as plt
+# keras imports
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import *
+from keras.optimizers import *
+from keras import backend as K 
+from keras.models import load_model
+from keras.utils.vis_utils import plot_model
+
+
+# reads in a video using opencv's VideoCapture object
+def read_video(filename):
+    # Create a VideoCapture object and read from input file
+    # ZB Edit: was getting exceptions when trying to open video so used full directory name
+    string = os.getcwd()
+    video = cv2.VideoCapture(string + '/' + filename)
+    # End ZB Edits
+    # Check if video opened successfully
+    if (video.isOpened() == False):
+        print("Error opening video stream or file")
+        return None
+    else:
+        # return video
+        return video
+
+# ------ from model.py ---------
+# iterate through frames of a video and call model.predict on each cell in a 16x9 grid
+# if smoke is identified then draw a green box on the cell, else draw red box
+# @params:
+# model - trained CNN model used to predict smoke results of grids
+# video - test video to run the network on
+# @returns:
+# nothing - displays results of grid calculation on local machine.
+def grid_contour(model, video):
+    done = False
+    while not done:
+        ret, curr_frame = video.read()
+        if not ret:
+            break
+        else:
+            # cv2.imshow("CurrentFrame", frame_copy)
+            frame_copy = curr_frame.copy()
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                done = True
+                break
+            for x in range(0, 16):
+                for y in range(0, 9):
+                    xg = x * 120
+                    yg = y * 120
+                    grid_segment = curr_frame[yg:yg + 120, xg:xg + 120]
+                    # resize and reshape the grid segment
+                    grid_segment = cv2.resize(grid_segment,(120, 120))
+                    grid_segment = np.reshape(grid_segment,[1,120,120,3])
+                    # predict the class
+                    classes = model.predict_classes(grid_segment)
+                    # get the class number
+                    max_index = classes[0][0]
+                    # edit the squares to be a pixel in
+                    start = (xg + 1, yg + 1)
+                    end = (xg + 119, yg + 119)
+                    if max_index == 1:
+                        color = (0, 255, 0)
+                        frame_copy = cv2.rectangle(frame_copy, start, end, color, 1)
+                    else:
+                        color = (0, 0, 255)
+                        frame_copy = cv2.rectangle(frame_copy, start, end, color, 2)
+            cv2.imshow("CurrentFrame", frame_copy)
+
+if __name__ == '__main__':
+  # get the model from training program
+  model = load_model('model_saved.h5')
+
+  model.compile(loss='binary_crossentropy',
+                optimizer='rmsprop',
+                metrics=['accuracy'])
+  
+  # print the model summary
+  print(model.summary())
+
+  # check that a video is passed in
+  if(len(sys.argv) > 1):
+    video = read_video(sys.argv[1])
+    grid_contour(model, video)
+    
+  else:
+    print("error: pass in the filepath of the video you want to test")
+
---
+++ keras_model_v2_train.py
@@ -0,0 +1,107 @@
+# model_gabe.py
+# 14 Apr 2020
+# implement with recent version of keras and tensorflow
+# code is mostly from: https://www.geeksforgeeks.org/python-image-classification-using-keras/
+
+import cv2
+import numpy as np
+import os
+from random import shuffle
+import matplotlib.pyplot as plt
+# keras imports
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import *
+from keras.optimizers import *
+from keras import backend as K 
+from keras.utils import plot_model
+
+
+if __name__ == '__main__':
+  # get training and validation set
+  img_width, img_height = 120, 120
+  
+  train_data_dir = 'training'
+  validation_data_dir = 'validation'
+  nb_train_samples = 16000
+  nb_validation_samples = 6000
+  epochs = 50
+  batch_size = 32
+  
+  if K.image_data_format() == 'channels_first': 
+    input_shape = (3, img_width, img_height) 
+  else: 
+    input_shape = (img_width, img_height, 3) 
+  
+  model = Sequential() 
+  # first conv
+  model.add(Conv2D(32, (2, 2), input_shape = input_shape)) 
+  model.add(Activation('relu')) 
+  model.add(MaxPooling2D(pool_size =(2, 2))) 
+  
+  # second conv
+  model.add(Conv2D(32, (2, 2))) 
+  model.add(Activation('relu')) 
+  model.add(MaxPooling2D(pool_size =(2, 2))) 
+  
+  # third conv
+  model.add(Conv2D(64, (2, 2))) 
+  model.add(Activation('relu')) 
+  model.add(MaxPooling2D(pool_size =(2, 2))) 
+  
+  model.add(Flatten()) 
+  model.add(Dense(64)) 
+  model.add(Activation('relu')) 
+  model.add(Dropout(0.5)) 
+  model.add(Dense(1)) 
+  model.add(Activation('sigmoid')) 
+  
+  model.compile(loss ='binary_crossentropy', 
+                optimizer ='rmsprop', 
+                metrics =['accuracy']) 
+  
+  train_datagen = ImageDataGenerator( 
+                    rescale = 1. / 255, 
+                    shear_range = 0.2, 
+                    zoom_range = 0.2, 
+                    horizontal_flip = True) 
+  
+  test_datagen = ImageDataGenerator(rescale = 1. / 255) 
+  
+  train_generator = train_datagen.flow_from_directory(train_data_dir, 
+                        target_size =(img_width, img_height), 
+                        batch_size = batch_size, class_mode ='binary') 
+  
+  validation_generator = test_datagen.flow_from_directory( 
+                                    validation_data_dir, 
+                                    target_size =(img_width, img_height), 
+                                    batch_size = batch_size, class_mode ='binary') 
+  
+  history = model.fit_generator(train_generator, 
+    steps_per_epoch = nb_train_samples // batch_size, 
+    epochs = epochs, validation_data = validation_generator, 
+    validation_steps = nb_validation_samples // batch_size) 
+  
+  # save the model
+  model.save('model_saved.h5')
+  
+  #### if you have graphviz then you can uncomment this line
+  # plot_model(model, to_file="keras_modelplot.png")
+
+  # Plot training & validation accuracy values
+  plt.plot(history.history['acc'])
+  plt.plot(history.history['val_acc'])
+  plt.title('Model accuracy')
+  plt.ylabel('Accuracy')
+  plt.xlabel('Epoch')
+  plt.legend(['Train', 'Test'], loc='upper left')
+  plt.show()
+
+  # Plot training & validation loss values
+  plt.plot(history.history['loss'])
+  plt.plot(history.history['val_loss'])
+  plt.title('Model loss')
+  plt.ylabel('Loss')
+  plt.xlabel('Epoch')
+  plt.legend(['Train', 'Test'], loc='upper left')
+  plt.show()
